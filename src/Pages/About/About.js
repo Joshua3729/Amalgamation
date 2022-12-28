069846400cc269ed26d2246fe4d04ba396853f2c
MM-Maketela <maketelamafa2@gmail.com>
2022-12-28 18:19:15 +0200

    about us updates

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,7 +1,12 @@
 import React from "react";
-
+import classes from  './About.module.css'
+import AboutUsSection1 from "./AboutUsSection1";
 const About = () => {
-  return <div>About</div>;
+    return <div
+        
+        className={classes.aboutUsWrapper}>
+        <AboutUsSection1/>
+    </div>;
 };
 
 export default About;
