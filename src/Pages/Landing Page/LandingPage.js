31f78cd956480bf5f046f4d676b9f004a36de929
Joshua <3729344@myuwc.ac.za>
2022-12-28 18:10:18 +0200

    added to landing page

--- src/Pages/Landing Page/LandingPage.js
+++ src/Pages/Landing Page/LandingPage.js
@@ -1,11 +1,12 @@
 import Hero from "../../Component/Hero/Hero";
 import classes from "./LandingPage.module.css";
+import WhoAreWe from "./WhoAreWe/WhoAreWe";
 
 const LandingPage = (props) => {
   return (
     <div className={classes.LandingPage}>
       <Hero />
-      <div className={classes.section2}></div>
+      <WhoAreWe />
     </div>
   );
 };
