9708b158af9048d6da8c31b4b78df715a86ab0b6
MM-Maketela <maketelamafa2@gmail.com>
2022-12-29 08:37:07 +0200

    about us updates

--- src/Pages/About/Founder.js
+++ src/Pages/About/Founder.js
@@ -2,7 +2,11 @@ import React from 'react'
 import classes from "./About.module.css";
 const Founder = () => {
   return (
-    <div className={classes.founderWrapper}>Founder</div>
+    <div className={classes.founderWrapper}>
+      <div className={classes.founderContainer}>
+
+      </div>
+    </div>
   )
 }
 
