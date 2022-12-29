c0ddf311e5706621c8bf6d9643c8f46edb0a0c13
Siphemkhosi <siphemkhosi7@gmail.com>
2022-12-30 00:31:47 +0200

    aaa

--- src/Pages/WhatWeDo/WhatWeDoSection3.js
+++ src/Pages/WhatWeDo/WhatWeDoSection3.js
@@ -1,50 +1,51 @@
-
 import Cards from "../WhatWeDo/Components/Card/Cards";
 import classes from "./WhatWeDo.module.css";
 import cars from "../../Assets/Images/catering.jpg";
+import beutique from "../../Assets/Images/Clothing.jpg";
 import React, { useState } from "react";
 import { PRIMARY_COLOR } from "../../Constants/Colors";
-import { AiOutlineRight } from 'react-icons/ai';
+import { AiOutlineRight } from "react-icons/ai";
 import { Link } from "react-router-dom";
 const WhatWeDoSection3 = () => {
   return (
     <>
-    
-      <div className={classes.mainContainerSection3}>
-        <div className={classes.imageContainer}>
-          <img src={cars} alt="cars" className={classes.firstImage} />
+    <h1 className={classes.title}>What We Do?</h1>
+      <div className={classes.mainContainerSection3a}>
+        <div className={classes.imageContainera}>
+          <img src={cars} alt="cars" className={classes.firstImagea} />
         </div>
-        <div className={classes.section3Div}>
-          <p className={classes.titleSection3}>Title</p>
-          <p className={classes.descriptionSection3}>
-            Lorem Ipsum is simply dummy text of the gtfd house printing and
-            typesettingindustry. Lorem Ipsum has been the industry's standard
-            dummy. NM Cuts Transport Lorem Ipsum is simply dummy text of the
-            gtfd house printing and typesettingindustry. Lorem Ipsum has been
-            the industry's standard dumm
+        <div className={classes.section3Diva}>
+          <p className={classes.titleSection3}>NM Cuts Catering</p>
+          <p className={classes.descriptionSection3a}>
+            We provide top quality catering services at affordable prices and
+            incredible food that will have your guests talking. Our food is our
+            business Each client is uniquely important to us. We work closely
+            with our customers to ensure every detail is correct. Our aim is to
+            deliver customer satisfaction through high quality food, and top
+            class service, at competitive and affordable prices We will create
+            the event you have dreamed about for your special day.
+           
           </p>
-          <div
-            className={classes.discoverMoreButton}
-            style={{
-              backgroundColor: PRIMARY_COLOR,
-              cursor: "pointer",
-            }}
-          >
-            <span>
-              <Link
-                to="about"
-                className={classes.link}
-                style={{
-                  textDecoration: "none",
-                }}
-              >
-                Discover more
-              </Link>
-            </span>
-            <AiOutlineRight size={20} />
-          </div>
         </div>
-      
+      </div>
+      <div className={classes.mainContainerSection3b}>
+      <div className={classes.section3Divb}>
+          <p className={classes.titleSection3}>NM Cuts Beautique</p>
+          <p className={classes.descriptionSection3b}>
+            We provide top quality catering services at affordable prices and
+            incredible food that will have your guests talking. Our food is our
+            business Each client is uniquely important to us. We work closely
+            with our customers to ensure every detail is correct. Our aim is to
+            deliver customer satisfaction through high quality food, and top
+            class service, at competitive and affordable prices We will create
+            the event you have dreamed about for your special day.
+           
+          </p>
+        </div>
+        <div className={classes.imageContainerb}>
+          <img src={beutique } alt="cars" className={classes.firstImageb} />
+        </div>
+ 
       </div>
     </>
   );
