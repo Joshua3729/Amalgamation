8e4fd813e34915e49624a74b97a508136780d929
Siphemkhosi <siphemkhosi7@gmail.com>
2022-12-29 15:00:02 +0200

    my changes

--- src/Pages/WhatWeDo/Components/Card/Cards.js
+++ src/Pages/WhatWeDo/Components/Card/Cards.js
@@ -1,52 +1,69 @@
-import React from 'react';
-import classes from './Cards.module.css';
-import CardItem from './CardItem';
-import CardItem2 from './CardItem2';
-import image1 from "../../../../Assets/Images/Clothing.jpg"
-import image2 from "../../../../Assets/Images/catering.jpg"
-import image3 from "../../../../Assets/Images/transport.webp"
-import image4 from "../../../../Assets/Images/couch.webp"
+import React from "react";
+import classes from "./Cards.module.css";
 
+import image1 from "../../../../Assets/Images/Clothing.jpg";
+import image2 from "../../../../Assets/Images/catering.jpg";
+import image3 from "../../../../Assets/Images/transport.webp";
+import image4 from "../../../../Assets/Images/couch.webp";
 
-function Cards() {
+const Cards = () => {
   return (
-    <div className={classes.cards}>
-      <h1>Our Product Services</h1>
-      <div className={classes.cards__container}>
-        <div className={classes.cards__wrapper}>
-          <ul className={classes.cards__items}>
-            <CardItem
-             src={image1}
-            />
-            <CardItem
-            src={image2}
-            />
-                <CardItem
-             src={image3}
-            />
-            <CardItem
-         src={image4}
-            />
-          </ul>
-          <ul className={classes.cards__items}>
-          <CardItem2
-              text='Get the latest fashion trends with NM Cuts Beautique'
-            />
-               <CardItem2
-              text='Get the fresh meat and best catering experience with NM Cuts Catering'
-            />
-               <CardItem2
-              text='Recieve the best travel experience with NM Cuts Transport'
-            />
-               <CardItem2
-              text='Experience the most comfortable  rests and home look  with NM Cuts Furniture'
-            />
-           
-          </ul>
+   <div className={classes.card}>
+      <h1>Our Service Products</h1>
+      <div className={classes.cards_container}>
+      <div className={classes.cards_wrapper}>
+      <div className={classes.cards}>
+        <div className={classes.cards_image}>
+          <img src={image1} />
+        </div>
+        <div>
+          <p className={classes.cards_header}>NM Cuts Beautique</p>
+          <p className={classes.cards_paragraph}>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy
+          </p>
         </div>
       </div>
-    </div>
+      <div className={classes.cards}>
+        <div className={classes.cards_image}>
+          <img src={image2} />
+        </div>
+        <div>
+          <p className={classes.cards_header}>NM Cuts Catering</p>
+          <p className={classes.cards_paragraph}>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy
+          </p>
+        </div>
+      </div>
+      <div className={classes.cards}>
+        <div className={classes.cards_image}>
+          <img src={image3} />
+        </div>
+        <div>
+          <p className={classes.cards_header}>NM Cuts Transport</p>
+          <p className={classes.cards_paragraph}>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy
+          </p>
+        </div>
+      </div>
+      <div className={classes.cards}>
+        <div className={classes.cards_image}>
+          <img src={image4} />
+        </div>
+        <div>
+          <p className={classes.cards_header}>NM Cuts Furniture</p>
+          <p className={classes.cards_paragraph}>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy
+          </p>
+        </div>
+      </div>
+      </div>
+      </div>
+</div>
   );
-}
+};
 
 export default Cards;
