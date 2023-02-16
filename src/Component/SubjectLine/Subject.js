e2886e06b17a28299ee542cb1b01c20ed738db76
Siphemkhosi <siphemkhosi7@gmail.com>
2023-02-16 21:46:57 +0200

    re styled wwd page

--- src/Component/SubjectLine/Subject.js
+++ src/Component/SubjectLine/Subject.js
@@ -5,18 +5,18 @@ const Subject = (props) => {
   return (
     <>
       <div className={classes.SubjectComponent}>
-        <div className={classes.question}>{props.Subject} </div>
-        <div className={classes.smallText}>
-        <h4>NM amalgeted|<b>Group PTY LTD</b></h4>
+        {/* <div className={classes.question}>{props.Subject} </div> */}
+        {/* <div className={classes.smallText}> */}
+        <h2>NM amalgeted|<b>Group PTY LTD</b></h2>
           <div className={classes.flexDiv}>
        
           <div className={classes.text} >
             <h3>Here's what we have to offer ...</h3>
-     <span><p><b>&#10003;</b> NM cuts  Transport Transport Catering</p></span>
-     <span><p><b>&#10003;</b> NM cuts  Transport Transport Furniture</p></span>
+     <span><p><b>&#10003;</b> NMcuts  Transport Transport Catering</p></span>
+     <span><p><b>&#10003;</b> NMcuts  Transport Transport Furniture</p></span>
  	
-   <p><b>&#10003;</b> NM cuts  Transport  TransportBeautique</p> 
-     <p><b>&#10003;</b> NM cuts  Transport  TransportTransport</p>  	
+   <p><b>&#10003;</b> NMcuts  Transport  TransportBeautique</p> 
+     <p><b>&#10003;</b> NMcuts  Transport  TransportTransport</p>  	
     <button>I'm ready to explore</button>
          </div>
           <div className={classes.image}>
@@ -24,7 +24,7 @@ const Subject = (props) => {
           </div>
           </div>
           </div>
-      </div>
+      {/* </div> */}
     </>
   );
 }
